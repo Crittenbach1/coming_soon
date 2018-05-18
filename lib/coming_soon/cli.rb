df7f05fab3ca8e5b02fc43570ab02ccea674d33b
Cynthia Rittenbach <cynthia@Cynthias-MacBook-Pro.local>
2018-05-18 13:55:42 -0400

    added welcome and list_movies methods

--- lib/coming_soon/cli.rb
+++ lib/coming_soon/cli.rb
@@ -1,7 +1,30 @@
 class ComingSoon::CLI
 
   def call
-  
+    welcome
+    list_movies
   end
+
+  def welcome
+    puts "In Theaters Now - Box Office Top Ten"
+    puts " "
+  end
+
+  def list_movies
+    puts "1. Avengers: Infinity War (2018)"
+    puts "2. Life of the Party (2018)"
+    puts "3. Life of the Party (2018)"
+    puts "4. Breaking In (2018)"
+    puts "5. Overboard (2018)"
+    puts "6. A Quiet Place (2018)"
+    puts "7. I Feel Pretty (2018)"
+    puts "8. Rampage (2018)"
+    puts "9. Tully (2018) (2018)"
+    puts "10. Black Panther (2018)"
+    puts "11. RBG (2018)"
+  end
+
+
+
 
 end
