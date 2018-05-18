e30106de5e16bc3944a3dfd6bf8f67dbc2e17933
Cynthia Rittenbach <cynthia@Cynthias-MacBook-Pro.local>
2018-05-18 17:33:33 -0400

    makes new movie instance for all movies on list

--- lib/coming_soon/movie.rb
+++ lib/coming_soon/movie.rb
@@ -5,7 +5,15 @@ class ComingSoon::Movie
        html = Nokogiri::HTML(open('https://www.imdb.com/movies-in-theaters/?ref_=cs_inth'))
 
        @list_items = html.css('div.list.detail.sub-list div.list_item')
-       
+
+       @list_items.each do |i|
+         @movies << @movie = TestNokogiri::Test.new
+         
+       end
+
+
+
+
    end
 
 end
