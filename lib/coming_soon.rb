71d1e598111eb0119341cb3e844d24752f73bf13
Cynthia Rittenbach <cynthia@Cynthias-MacBook-Pro.local>
2018-05-18 17:49:51 -0400

    fixed path to file

--- lib/coming_soon.rb
+++ lib/coming_soon.rb
@@ -3,5 +3,5 @@ require "nokogiri"
 require "pry"
 
 require_relative "coming_soon/version"
-require_relative "coming_soon/movie"
+require_relative "./coming_soon/movie"
 require_relative "./coming_soon/cli"
