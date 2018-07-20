328d151894ddf666a94177a297ff406d38face18
Cynthia Rittenbach <crittenbach@gmail.com>
2018-07-20 00:31:16 -0400

    forgot @ on variable call

--- lib/coming_soon/movie.rb
+++ lib/coming_soon/movie.rb
@@ -16,7 +16,7 @@ class ComingSoon::Movie
        @list_items = html.css('div.list.detail.sub-list div.list_item')
 
        @list_items.each do |i|
-         @all << @movie = ComingSoon::Movie.new
+         @@all << @movie = ComingSoon::Movie.new
          @movie.title = i.css("h4").text.strip
          @movie.time = i.css("time").text.strip
          @movie.description = i.css(".outline").text.strip
